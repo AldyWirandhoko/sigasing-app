d5aadb78dde4ffdbb30f4dcbe200ddbe590dd6e9
Aldy Wirandhoko <aldywirandhoko@gmail.com>
2024-01-07 18:59:43 +0800

    Merapikan footer, nav, dan sidebar

--- partials/footer.php
+++ partials/footer.php
@@ -1,9 +1,9 @@
 <!-- Main Footer -->
 <footer class="main-footer">
     <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-    Anything you want
+    <div class="float-right d-none d-sm-block">
+    <b>Version</b> 0.1.0
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2024 <a href="#">Praktikum Web</a>.</strong> All rights reserved.
 </footer>
