5cf7e882c8172e170f2543ef1104719953aded2d
Aldy Wirandhoko <aldywirandhoko@gmail.com>
2024-01-07 21:42:04 +0800

    Menampilkan data lokasi

--- pages/admin/lokasiread.php
+++ pages/admin/lokasiread.php
@@ -1,57 +1,81 @@
 <?php include_once "partials/cssdatatables.php" ?>
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">Lokasi</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Lokasi</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Lokasi</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Lokasi</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
 
-    <!-- Main content -->
-    <div class="content">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Data Lokasi</h3>
-                <a href="?page=lokasicreate" class="btn btn-success btn-sm float-right">
-                    <i class="fa fa-plus-circle"></i> Tambah data</a>
-            </div>
-            <div class="card-body">
-                <table id="mytable" class="table table-bordered table-hover">
-                    <thead>
+<!-- Main content -->
+<div class="content">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Data Lokasi</h3>
+            <a href="?page=lokasicreate" class="btn btn-success btn-sm float-right">
+                <i class="fa fa-plus-circle"></i> Tambah data</a>
+        </div>
+        <div class="card-body">
+            <table id="mytable" class="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama Lokasi</th>
+                        <th>Opsi</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama Lokasi</th>
+                        <th>Opsi</th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                    <?php
+                    $database = new Database();
+                    $db = $database->getConnection();
+                    $selectSql = "SELECT * FROM lokasi";
+                    $stmt = $db->prepare($selectSql);
+                    $stmt->execute();
+                    $no = 1;
+                    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    ?>
                         <tr>
-                            <th>No</th>
-                            <th>Nama Lokasi</th>
-                            <th>Opsi</th>
+                            <td><?php echo $no++ ?></td>
+                            <td><?php echo $row['nama_lokasi'] ?></td>
+                            <td>
+                                <a href="?page=lokasiupdate&id=<?php echo $row['id'] ?>" class="btn btn-primary btn-sm mr-1">
+                                    <i class="fa fa-edit"></i> Ubah
+                                </a>
+                                <a href="?page=lokasidelete&id=<?php echo $row['id'] ?>" class="btn btn-danger btn-sm" onClick="javascript: return confirm('Konfirmasi data akan dihapus?');">
+                                    <i class="fa fa-trash"></i> Hapus
+                                </a>
+                            </td>
                         </tr>
-                    </thead>
-                    <tfoot>
-                        <tr>
-                            <th>No</th>
-                            <th>Nama Lokasi</th>
-                            <th>Opsi</th>
-                        </tr>
-                    </tfoot>
-                    <tbody>
-                    </tbody>
-                </table>
-            </div>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </div>
-    <!-- /.content -->
-    <?php include "partials/scripts.php" ?>
-    <?php include "partials/scriptsdatatables.php" ?>
-    <script>
-        $(function() {
-            $('#mytable').DataTable()
-        });
-    </script>
+</div>
+<!-- /.content -->
+<?php include "partials/scripts.php" ?>
+<?php include "partials/scriptsdatatables.php" ?>
+<script>
+    $(function() {
+        $('#mytable').DataTable()
+    });
+</script>
