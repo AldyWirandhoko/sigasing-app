1c575df1e1eecf9612b5d15fdee9b140fc946683
Aldy Wirandhoko <aldywirandhoko@gmail.com>
2024-01-12 09:18:39 +0800

    Membuat Halaman Tambah Data

---
+++ pages/admin/lokasicreate.php
@@ -0,0 +1,50 @@
+<?php include_once "partials/cssdatatables.php" ?>
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Tambah Data Lokasi</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="?page=home">Home</a></li>
+                    <li class="breadcrumb-item"><a href="?page=lokasiread">Lokasi</a></li>
+                    <li class="breadcrumb-item active">Tambah Data</li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<!-- Main content -->
+<div class="content">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Tambah Lokasi</h3>
+        </div>
+        <div class="card-body">
+            <form method="POST">
+                <div class="form-group">
+                    <label for="nama_lokasi">Nama Lokasi</label>
+                    <input type="text" class="form-control" name="nama_lokasi">
+                </div>
+                <a href="?page=lokasiread" class="btn btn-danger btn-sm float-right">
+                    <i class="fa fa-times"></i> Batal
+                </a>
+                <button type="submit" name="button_create" class="btn btn-success btn-sm float-right">
+                    <i class="fa fa-save"></i> Simpan
+                </button>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- /.content -->
+<?php include "partials/scripts.php" ?>
+<?php include "partials/scriptsdatatables.php" ?>
+<script>
+    $(function() {
+        $('#mytable').DataTable()
+    });
+</script>
--- routes.php
+++ routes.php
@@ -9,6 +9,9 @@ if (isset($_GET['page'])){
         case 'lokasiread':
             file_exists('pages/admin/lokasiread.php') ? include 'pages/admin/lokasiread.php' : include "pages/404.php";
             break;
+        case 'lokasicreate':
+            file_exists('pages/admin/lokasicreate.php') ? include 'pages/admin/lokasicreate.php' : include "pages/404.php";
+            break;
         default:
             include "pages/404.php";
         }
