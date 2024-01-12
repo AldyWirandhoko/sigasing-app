8f062cfdf8194d81e4482f3b2b4cca53cc8554c0
Aldy Wirandhoko <aldywirandhoko@gmail.com>
2024-01-13 02:28:44 +0800

    Membuat Perintah Create Data

--- pages/admin/lokasicreate.php
+++ pages/admin/lokasicreate.php
@@ -1,5 +1,37 @@
-<?php include_once "partials/cssdatatables.php" ?>
-<!-- Content Header (Page header) -->
+<?php
+if (isset($_POST['button_create'])) {
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $validateSql = "SELECT * FROM lokasi WHERE nama_lokasi = ?";
+    $stmt = $db->prepare($validateSql);
+    $stmt->bindParam(1, $_POST['nama_lokasi']);
+    $stmt->execute();
+    if($stmt->rowCount() > 0){
+?>
+        <div class="alert alert-danger alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
+            <h5><i class="icon fas fa-ban"></i>Gagal</h5>
+            Nama lokasi sama sudah ada
+        </div>
+<?php
+    } else {
+        $insertSQL = "INSERT INTO lokasi SET nama_lokasi = ?";
+        $stmt = $db->prepare($insertSQL);
+        $stmt ->bindParam(1, $_POST['nama_lokasi']);
+        if ($stmt->execute()) {
+            $_SESSION['hasil'] = true;
+            $_SESSION['pesan'] = "Berhasil simpan data";
+        } else {
+            $_SESSION['hasil'] = false;
+            $_SESSION['pesan'] = "Gagal simpan data";
+        }
+        echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'>";
+    }
+}
+?>
+
 <div class="content-header">
     <div class="container-fluid">
         <div class="row mb-2">
@@ -42,9 +74,3 @@
 </div>
 <!-- /.content -->
 <?php include "partials/scripts.php" ?>
-<?php include "partials/scriptsdatatables.php" ?>
-<script>
-    $(function() {
-        $('#mytable').DataTable()
-    });
-</script>
--- pages/admin/lokasiread.php
+++ pages/admin/lokasiread.php
@@ -2,6 +2,29 @@
 <!-- Content Header (Page header) -->
 <div class="content-header">
     <div class="container-fluid">
+        <?php
+        if (isset($_SESSION["hasil"])) {
+            if ($_SESSION["hasil"]) {
+        ?>
+                <div class="alert alert-success alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                    <h5><i class="icon fas fa-check"></i> Berhasil</h5>
+                    <?php echo $_SESSION["pesan"] ?>
+                </div>
+            <?php
+            } else {
+            ?>
+                <div class="alert alert-danger alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                    <h5><i class="icon fas fa-ban"></i> Gagal</h5>
+                    <?php echo $_SESSION["pesan"] ?>
+                </div>
+        <?php
+            }
+            unset($_SESSION['hasil']);
+            unset($_SESSION['pesan']);
+        }
+        ?>
         <div class="row mb-2">
             <div class="col-sm-6">
                 <h1 class="m-0">Lokasi</h1>
