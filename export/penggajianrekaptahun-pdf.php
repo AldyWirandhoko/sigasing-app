1d8364c5aa591dfed4fcc52c443c34878506fcca
Aldy Wirandhoko <aldywirandhoko@gmail.com>
2024-01-24 15:06:34 +0800

    Ekspor Rekapitulasi PerTahun  ke PDF menggunakan Html2pdf

---
+++ export/penggajianrekaptahun-pdf.php
@@ -0,0 +1,24 @@
+<?php 
+
+require_once '../vendor/autoload.php';
+
+use Spipu\Html2Pdf\Html2Pdf;
+use Spipu\Html2Pdf\Exception\Html2PdfException;
+use Spipu\Html2Pdf\Exception\ExceptionFormatter;
+
+try {
+    ob_start();
+    include 'res/penggajianrekaptahun-res.php';
+    $content = ob_get_clean();
+
+    $html2pdf = new Html2Pdf('P', 'A4', 'en');
+    $html2pdf->pdf->setDisplayMode('fullpage');
+    $html2pdf->writeHTML($content);
+    $html2pdf->output('Rekap_Penggajian_Pertahun.pdf');
+} catch (Html2PdfException $e) {
+    $html2pdf->clean();
+
+    $formatter = new ExceptionFormatter($e);
+    echo $formatter->getMessage();
+}
+?>
